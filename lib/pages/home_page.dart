c16ea6415c28b99105ea7851e1f29e17116df500
Kudav5 <kdvalerian@gmail.com>
2026-01-02 14:10:47 +0700

    side panel update

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,16 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:smart_tools/data/gridItems_data.dart';
 
 class MyHome extends StatelessWidget {
-  const MyHome({super.key});
+  //const MyHome({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(width: 230, elevation: 10, shadowColor: Colors.cyan),
+      drawer: Drawer(width: 230, elevation: 10, shadowColor: Colors.cyan, child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(padding: EdgeInsetsGeometry.all(10.0)),
+          Row(
+            children: [
+              Image(image: AssetImage("assets/img/app_icon.png")),
+             SizedBox(width: 10,),
+              Column(
+                children: [
+                  Text("Smart Tools"),
+                  Text("Version: 1.0")
+                ],
+              ),
+            ],
+          ),
+          SizedBox(height: 5),
+          Row(
+            children: [
+              Icon(FontAwesomeIcons.circleInfo),
+              SizedBox(width: 10,),
+              TextButton(onPressed: () {}, child: Text("About Us"))
+            ],
+          ),
+          SizedBox(height: 5),
+          Row(
+            children: [
+              Icon(FontAwesomeIcons.message),
+              SizedBox(width: 10,),
+              TextButton(onPressed: () {}, child: Text("Feedback"))
+            ],
+          ),
+          SizedBox(height: 5),
+          Row(
+            children: [
+              Icon(FontAwesomeIcons.share),
+              SizedBox(width: 10,),
+              TextButton(onPressed: () {}, child: Text("Share"))
+            ],
+          ),
+          SizedBox(height: 5),
+          Row(
+            children: [
+              Icon(FontAwesomeIcons.language),
+              SizedBox(width: 10,),
+              TextButton(onPressed: () {}, child: Text("Language"))
+            ],
+          ),
+          
+        ],
+      ),),
 
       appBar: AppBar(
-        title: Text("Smart Tools", style: TextStyle(color: Colors.black)),
+        title: Text("Smart Tools", style: TextStyle(color: Colors.white)),
         backgroundColor: const Color.fromARGB(184, 0, 187, 212),
       ),
 
@@ -56,7 +108,7 @@ Widget boxes(GridItem item, context) {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Expanded(
-                    child: Icon(item.icon, size: 40, color: item.iconColor),
+                    child: Icon(item.icon, size: 40, color: Colors.amber),
                   ),
                 ],
               ),
